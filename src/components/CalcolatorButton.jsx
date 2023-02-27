1c96be161ca3809f5d62625465db66e73631ac1b
ahmad alsholi <ahmad.alsholy@soicumtech.com>
2023-02-27 07:54:47 +0300

    inital commit

--- src/components/CalcolatorButton.jsx
+++ src/components/CalcolatorButton.jsx
@@ -1,43 +1,70 @@
 import React, { Fragment } from "react";
 import { useState } from "react";
 import classes from "./Calculator.module.css";
+import { evaluate } from "mathjs";
 
-const CalButton = () => {
-  const [num, setnum] = useState("");
-  const [FirstNum, setFirstNum] = useState("");
-  const [operation, setOperation] = useState("");
+const CalButton = ({ updateNumValue }) => {
+  const [num, setNum] = useState(0);
+  const [FirstNum, setFirstNum] = useState(0);
+  const [operation, setOperation] = useState();
+  const [result, setResult] = useState();
 
   const getnum = (event) => {
-    setnum(event.target.value)
+    let input = event.target.value
+    if (num === 0) {
+      setNum(input);
+    } else {
+      setNum(num + input);
+    }
+    updateNumValue(num)
   };
   console.log('num: ', num);
 
+  const operator = (e) => {
+    let operatorInput = e.target.value;
+    setOperation(operatorInput);
+    setFirstNum(num);
+    setNum(0);
+  }
+  const clear = () => {
+    setNum(0)
+  }
+
+  const calculate = () => {
+    const result = evaluate(`${FirstNum}${operation}${num}`);
+    setNum(result);
+  }
   return (
     <Fragment>
       <div className={classes.allButton}>
         <div>
-          <button onClick={getnum} value='7'>7</button>
-          <button onClick={getnum} value='8'>8</button>
-          <button onClick={getnum} value='9'>9</button>
-          <button onClick={getnum} value='x'>x</button>
+          <button onClick={clear}>AC</button>
+          <button onClick={getnum}>+/-</button>
+          <button onClick={getnum}>%</button>
         </div>
         <div>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button onClick={getnum} value={7}>7</button>
+          <button onClick={getnum} value={8}>8</button>
+          <button onClick={getnum} value={9}>9</button>
+          <button onClick={operator} value={'*'}>x</button>
         </div>
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
+          <button onClick={getnum} value={4}>4</button>
+          <button onClick={getnum} value={5}>5</button>
+          <button onClick={getnum} value={6}>6</button>
+          <button onClick={operator} value={'-'}>-</button>
         </div>
         <div>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
-          <button>/</button>
+          <button onClick={getnum} value={1}>1</button>
+          <button onClick={getnum} value={2}>2</button>
+          <button onClick={getnum} value={3}>3</button>
+          <button onClick={operator} value={'+'}>+</button>
+        </div>
+        <div>
+          <button onClick={getnum} value={0}>0</button>
+          <button onClick={getnum} value={'.'}>.</button>
+          <button onClick={calculate} value={'='}>=</button>
+          <button onClick={operator} value={'/'}>/</button>
         </div>
       </div>
     </Fragment>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,20 @@
-import React, { Fragment } from "react";
-import CalResult from "./CalculatorResult";
+import React, { Fragment,useState } from "react";
 import CalButton from "./CalcolatorButton";
-import CalInput from "./CalculatorInput";
 import classes from "./Calculator.module.css";
 const Calculator = () => {
+  const [numValue, setNumValue] = useState(0);
+
+  const updateNumValue = (value) => {
+    setNumValue(value);
+  }
   return (
     <Fragment>
       <div className={classes.mainCal}>
-        <CalInput />
-        <CalResult />
-        <CalButton />
+
+        <p>{numValue}</p>
+        <p>{}</p>
+
+        <CalButton updateNumValue={updateNumValue}  />
       </div>
     </Fragment>
   );
