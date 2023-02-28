f77e39c3fc62b47c3cb592df4bccc1e21e4bc69f
ahmad alsholi <ahmad.alsholy@soicumtech.com>
2023-02-28 08:22:54 +0300

    inital commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -10,7 +10,7 @@ const Calculator = () => {
   const [FirstNum, setFirstNum] = useState("");
   const [operation, setOperation] = useState("");
   const [result, setResult] = useState("");
-  const [lastOp,setLastOp] = useState('')
+  const [history, setHistory] = useState([]);
   // const updateNumValue = (value) => {
   //   setNumValue((prevState) => prevState + value);
   // };
@@ -23,19 +23,32 @@ const Calculator = () => {
     }
   };
   const clear = () => {
-    setNumValue(0);
-    setResult(0);
+    setNumValue('');
+    setResult('');
     setOperation("");
   };
   const operator = (value) => {
+    if (numValue !== '') {
+      calculate()
+    }
     setOperation(value);
-    setFirstNum(numValue);
+    setFirstNum((prevFirstNum) => {
+      if (prevFirstNum === '') {
+        return numValue;
+      } else {
+        return evaluate(`${prevFirstNum}${operation}${numValue}`);
+      }
+    });
     setNumValue("");
   };
   const calculate = () => {
-  
     const result = evaluate(`${FirstNum}${operation}${numValue}`);
     setResult(result);
+    setHistory([...history, { result, operation: `${FirstNum} ${operation} ${numValue}` }]);
+    clear();
+  };
+  const handleHistoryClick = (index) => {
+    setHistory(history.slice(0, index + 1));
   };
   console.log("result: ", result);
   console.log("numValue: ", numValue);
@@ -43,7 +56,7 @@ const Calculator = () => {
     <Fragment>
       <div className={classes.mainCal}>
         <div className={classes.HistoryBox}>
-          <History lastResult={result} />
+          <History history={history} handleHistoryClick={handleHistoryClick} />
         </div>
         <div className={classes.calculaterBox}>
           <div>
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,12 +1,18 @@
 import React, { Fragment } from "react";
-const History = ({lastResult ,}) => {
+const History = ({ history, handleHistoryClick }) => {
   return (
     <Fragment>
       <div>
         <p>Operation History</p>
       </div>
       <div>
-        {lastResult}
+        <ul>
+          {history.map((entry, index) => (
+            <li key={index} onClick={() => handleHistoryClick(index)}>
+              {entry.operation} = {entry.result}
+            </li>
+          ))}
+        </ul>
       </div>
     </Fragment>
   );
